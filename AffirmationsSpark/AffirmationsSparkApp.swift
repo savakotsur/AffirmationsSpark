ab388cd8e6f7c1161d9ce2ee7966f09c3ea94462
savakotsur <slendygood@gmail.com>
2024-07-31 23:10:44 +0300

    Initial Commit

---
+++ AffirmationsSpark/AffirmationsSparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsSparkApp.swift
+//  AffirmationsSpark
+//
+//  Created by Савелий Коцур on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationsSparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
